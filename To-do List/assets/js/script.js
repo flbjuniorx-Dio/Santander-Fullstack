edc45078a34cbdc12077e054b5f5e56df9d8992c
flbjuniorx <flbjuniorx@gmail.com>
2022-06-17 16:47:47 -0300

    Correção

--- To-do List/assets/js/script.js
+++ To-do List/assets/js/script.js
@@ -1,7 +1,29 @@
-let tasks = [];
-let task = String;
+const taskform = document.getElementById('taskForm');
+const taskList = document.getElementById('taskList');
 
-function addTask(task) {
-    task = document.querySelector;
-    tasks.push(task);
+taskform.onsubmit = function (event) {
+    event.preventDefault();
+    const inputField = document.getElementById('taskBox');
+    inputField.value == "" ? alert("Digite a descrição da tarefa."): addTask(inputField.value);
+    form.reset();
+}
+
+function addTask(description) {
+    const taskContainer = document.createElement('div');
+    const newTask = document.createElement('input');
+    const taskLabel = document.createElement('label');
+    const taskDescriptionNode = document.createTextNode(description);
+
+    newTask.setAttribute('type','checkbox');
+    newTask.setAttribute('name',description);
+    newTask.setAttribute('id',description)
+
+    taskLabel.setAttribute('for',description);
+    taskLabel.appendChild(taskDescriptionNode);
+
+    taskContainer.classList.add('taskItem');
+    taskContainer.appendChild(newTask);
+    taskContainer.appendChild(taskLabel);
+
+    taskList.appendChild(taskContainer);
 }
